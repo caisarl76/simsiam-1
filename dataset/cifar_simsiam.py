a03b51cc3f8c827a0eed538e61fa282c6fe95d9d
caisarl76 <caisarl76@gmail.com>
2021-08-14 18:29:31 +0900

    add cifar-lt train code

---
+++ dataset/cifar_simsiam.py
@@ -0,0 +1,187 @@
+"""
+Adopted from https://github.com/Megvii-Nanjing/BBN
+Customized by Kaihua Tang
+"""
+
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+import random
+import torch
+import torchvision.datasets as datasets
+from simsiam.loader import GaussianBlur, TwoCropsTransform
+
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+# MoCo v2's aug: similar to SimCLR https://arxiv.org/abs/2002.05709
+augmentation = [
+    transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
+    transforms.RandomApply([
+        transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+    ], p=0.8),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize
+    ]
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    normalize
+])
+
+class IMBALANCECIFAR10(torchvision.datasets.CIFAR10):
+    cls_num = 10
+
+    def __init__(self, phase, imbalance_ratio, root='./data', imb_type='exp', simsiam=True, test_imb_ratio=None, reverse=False):
+        train = True if phase == "train" else False
+
+        super(IMBALANCECIFAR10, self).__init__(root, train, transform=None, target_transform=None, download=True)
+        self.train = train
+        self.simsiam = simsiam
+        if self.train:
+            self.img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imbalance_ratio,
+                                                         reverse=reverse)
+            self.gen_imbalanced_data(self.img_num_list)
+            self.loader = TwoCropsTransform(transforms.Compose(augmentation))
+        else:
+            if test_imb_ratio:
+                # if test imbalance ratio is explicitly given, test dataset should be imbalanced.
+                self.img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, test_imb_ratio,
+                                                             reverse=reverse)
+                self.gen_imbalanced_data(self.img_num_list)
+            else:
+                self.img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor=1.0,
+                                                             reverse=reverse)
+
+            self.transform = transforms.Compose([
+                             #transforms.Resize(224),
+                             transforms.ToTensor(),
+                             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                            ])
+
+        self.labels = self.targets
+
+        print("{} Mode: Contain {} images".format(phase, len(self.data)))
+
+    def _get_class_dict(self):
+        class_dict = dict()
+        for i, anno in enumerate(self.get_annotations()):
+            cat_id = anno["category_id"]
+            if not cat_id in class_dict:
+                class_dict[cat_id] = []
+            class_dict[cat_id].append(i)
+        return class_dict
+
+
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor, reverse=False):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                if reverse:
+                    num = img_max * (imb_factor**((cls_num - 1 - cls_idx) / (cls_num - 1.0)))
+                else:
+                    num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        elif imb_type == 'step':
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max))
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max * imb_factor))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+
+    def __getitem__(self, index):
+        img, label = self.data[index], self.labels[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+
+        if self.simsiam:
+            x1, x2 = self.loader(img)
+            return x1, x2
+        else:
+            if self.transform is not None:
+                img = self.transform(img)
+            if self.target_transform is not None:
+                label = self.target_transform(label)
+
+            # return img, label, index
+            return img, label
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_num_classes(self):
+        return self.cls_num
+
+    def get_annotations(self):
+        annos = []
+        for label in self.labels:
+            annos.append({'category_id': int(label)})
+        return annos
+
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
+
+
+class IMBALANCECIFAR100(IMBALANCECIFAR10):
+    """`CIFAR100 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
+    This is a subclass of the `CIFAR10` Dataset.
+    """
+    cls_num = 100
+    base_folder = 'cifar-100-python'
+    url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
+    filename = "cifar-100-python.tar.gz"
+    tgz_md5 = 'eb9058c3a382ffc7106e4002c42a8d85'
+    train_list = [
+        ['train', '16019d7e3df5f24257cddd939b257f8d'],
+    ]
+
+    test_list = [
+        ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
+    ]
+    meta = {
+        'filename': 'meta',
+        'key': 'fine_label_names',
+        'md5': '7973b15100ade9c7d40fb424638fde48',
+    }
+
+
+if __name__=="__main__":
+
+    # train = IMBALANCECIFAR100(phase='train', imbalance_ratio=0.1, root='./data', imb_type='exp')
+    train = datasets.CIFAR100('./data', train=True, download=True, transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(train, batch_size=100, shuffle=True,
+                                               num_workers=0, pin_memory=True)
+
+    for i, (input, target) in enumerate(train_loader):
+        print(i, input.shape, target)
+        break
---
+++ simsiam/builder.py
@@ -0,0 +1,131 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+
+
+class SimSiam(nn.Module):
+    """
+    Build a SimSiam model.
+    """
+    def __init__(self, base_encoder, dim=2048, pred_dim=512):
+        """
+        dim: feature dimension (default: 2048)
+        pred_dim: hidden dimension of the predictor (default: 512)
+        """
+        super(SimSiam, self).__init__()
+
+        # create the encoder
+        # num_classes is the output fc dimension, zero-initialize last BNs
+        self.encoder = base_encoder(num_classes=dim, zero_init_residual=True)
+
+        # build a 3-layer projector
+        prev_dim = self.encoder.fc.weight.shape[1]
+        self.encoder.fc = nn.Sequential(nn.Linear(prev_dim, prev_dim, bias=False),
+                                        nn.BatchNorm1d(prev_dim),
+                                        nn.ReLU(inplace=True), # first layer
+                                        nn.Linear(prev_dim, prev_dim, bias=False),
+                                        nn.BatchNorm1d(prev_dim),
+                                        nn.ReLU(inplace=True), # second layer
+                                        self.encoder.fc,
+                                        nn.BatchNorm1d(dim, affine=False)) # output layer
+        self.encoder.fc[6].bias.requires_grad = False # hack: not use bias as it is followed by BN
+
+        # build a 2-layer predictor
+        self.predictor = nn.Sequential(nn.Linear(dim, pred_dim, bias=False),
+                                        nn.BatchNorm1d(pred_dim),
+                                        nn.ReLU(inplace=True), # hidden layer
+                                        nn.Linear(pred_dim, dim)) # output layer
+
+    def forward(self, x1, x2):
+        """
+        Input:
+            x1: first views of images
+            x2: second views of images
+        Output:
+            p1, p2, z1, z2: predictors and targets of the network
+            See Sec. 3 of https://arxiv.org/abs/2011.10566 for detailed notations
+        """
+
+        # compute features for one view
+        z1 = self.encoder(x1) # NxC
+        z2 = self.encoder(x2) # NxC
+
+        p1 = self.predictor(z1) # NxC
+        p2 = self.predictor(z2) # NxC
+
+        return p1, p2, z1.detach(), z2.detach()
+
+class SimSiam_cifar(nn.Module):
+    """
+    Build a SimSiam model.
+    """
+    def __init__(self, base_encoder, dim=100, pred_dim=64):
+        """
+        dim: feature dimension (default: 100)
+        pred_dim: hidden dimension of the predictor (default: 64)
+        """
+        super(SimSiam_cifar, self).__init__()
+
+        # create the encoder
+        # num_classes is the output fc dimension, zero-initialize last BNs
+        self.encoder = base_encoder(num_classes=dim)
+
+        # build a 3-layer projector
+        prev_dim = self.encoder.linear.weight.shape[1]
+        self.encoder.linear = nn.Sequential(nn.Linear(prev_dim, prev_dim, bias=False),
+                                        nn.BatchNorm1d(prev_dim),
+                                        nn.ReLU(inplace=True), # first layer
+                                        nn.Linear(prev_dim, prev_dim, bias=False),
+                                        nn.BatchNorm1d(prev_dim),
+                                        nn.ReLU(inplace=True), # second layer
+                                        self.encoder.linear,
+                                        nn.BatchNorm1d(dim, affine=False)) # output layer
+        self.encoder.linear[6].bias.requires_grad = False # hack: not use bias as it is followed by BN
+
+        # build a 2-layer predictor
+        self.predictor = nn.Sequential(nn.Linear(dim, pred_dim, bias=False),
+                                        nn.BatchNorm1d(pred_dim),
+                                        nn.ReLU(inplace=True), # hidden layer
+                                        nn.Linear(pred_dim, dim)) # output layer
+
+    def forward(self, x1, x2):
+        """
+        Input:
+            x1: first views of images
+            x2: second views of images
+        Output:
+            p1, p2, z1, z2: predictors and targets of the network
+            See Sec. 3 of https://arxiv.org/abs/2011.10566 for detailed notations
+        """
+
+        # compute features for one view
+        z1 = self.encoder(x1) # NxC
+        z2 = self.encoder(x2) # NxC
+
+        p1 = self.predictor(z1) # NxC
+        p2 = self.predictor(z2) # NxC
+
+        return p1, p2, z1.detach(), z2.detach()
+
+def simsiam_resnet32(num_classes=10):
+    from simsiam.resnet_cifar import resnet32
+    return SimSiam_cifar(resnet32, dim=num_classes)
+
+def simsiam_resnet56(num_classes=10):
+    from simsiam.resnet_cifar import resnet56
+    return SimSiam_cifar(resnet56, dim=num_classes)
+
+
+
+if __name__ == "__main__":
+    from simsiam.resnet_cifar import resnet32
+    base_model = resnet32
+    model = SimSiam_cifar(base_model, dim=100)
+    print(model)
+    data = torch.randn([16, 3, 32, 32])
+    p1, p2, z1, z2 = model(data, data)
