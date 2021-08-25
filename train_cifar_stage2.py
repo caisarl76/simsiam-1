84fd14bd196487a789a022240326f9883e1134a2
caisarl76 <caisarl76@gmail.com>
2021-08-25 11:01:15 +0900

    change main_lincls adjust_lr code

--- train_cifar_stage2.py
+++ train_cifar_stage2.py
@@ -120,7 +120,7 @@ def main():
         print('train with supervised')
         stage2_fol = 'unsup'
     args.save_path = save_path = os.path.join(args.pretrained.split('checkpoint')[0].replace('stage1', stage2_fol), args.dataset,
-                                              (args.loss_type, (str)(args.epochs) + '_' + (str)(args.lr) + '_' + args.lr_sche))
+                                              '_'.join([args.loss_type, (str)(args.epochs), (str)(args.lr), args.lr_sche]))
     print(args.save_path, save_path)
     if not os.path.exists(save_path):
         os.makedirs(save_path, exist_ok=True)
